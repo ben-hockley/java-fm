bd7ef7fb92ea7d9a58838fab72bd590cf8a1f12f
Ben Hockley <hockleyb@cardiff.ac.uk>
2024-05-18 22:00:56 +0100

    Colour coded positions and added error handling for different positions

--- src/JFrames/manageTeam.java
+++ src/JFrames/manageTeam.java
@@ -5,6 +5,7 @@ import Objects.Team;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Objects;
 
 public class manageTeam extends JFrame {
 
@@ -14,8 +15,6 @@ public class manageTeam extends JFrame {
         this.setLayout(new GridLayout(1,2));
 
         JPanel startingElevenPanel = new JPanel();
-        startingElevenPanel.setBackground(new java.awt.Color(40, 140, 40));
-        startingElevenPanel.setForeground(new java.awt.Color(255, 255, 255));
 
         startingElevenPanel.setLayout(new GridLayout(13,1));
         startingElevenPanel.add(new JLabel("Starting 11:"));
@@ -24,6 +23,17 @@ public class manageTeam extends JFrame {
 
         for (Player player : userTeam.getStartingEleven()) {
             JRadioButton playerButton = new JRadioButton(player.getPlayerName());
+
+            if (Objects.equals(player.getPosition(), "GK")){
+                playerButton.setBackground(Color.ORANGE);
+            } else if (Objects.equals(player.getPosition(), "DEF")){
+                playerButton.setBackground(Color.YELLOW);
+            } else if (Objects.equals(player.getPosition(), "MID")){
+                playerButton.setBackground(Color.GREEN);
+            } else if (Objects.equals(player.getPosition(), "FWD")){
+                playerButton.setBackground(Color.CYAN);
+            }
+
             playerButton.setActionCommand(player.getPlayerName());
             startingEleven.add(playerButton);
             startingElevenPanel.add(playerButton);
@@ -32,9 +42,6 @@ public class manageTeam extends JFrame {
         startingElevenPanel.setOpaque(true);
 
         JPanel benchPanel = new JPanel();
-
-        benchPanel.setBackground(Color.GRAY);
-        benchPanel.setForeground(Color.BLUE);
         benchPanel.setLayout(new GridLayout(13,1));
         benchPanel.add(new JLabel("Substitutes:"));
 
@@ -42,25 +49,46 @@ public class manageTeam extends JFrame {
 
         for (Player player : userTeam.getSubstitutes()) {
             JRadioButton playerButton = new JRadioButton(player.getPlayerName());
+
+            if (Objects.equals(player.getPosition(), "GK")){
+                playerButton.setBackground(Color.ORANGE);
+            } else if (Objects.equals(player.getPosition(), "DEF")){
+                playerButton.setBackground(Color.YELLOW);
+            } else if (Objects.equals(player.getPosition(), "MID")){
+                playerButton.setBackground(Color.GREEN);
+            } else if (Objects.equals(player.getPosition(), "FWD")){
+                playerButton.setBackground(Color.CYAN);
+            }
+
             playerButton.setActionCommand(player.getPlayerName());
             substitutes.add(playerButton);
             benchPanel.add(playerButton);
         }
 
-        Button printSelected = new Button("Substitute Player");
-        printSelected.setBackground(Color.BLUE);
-        printSelected.setForeground(Color.WHITE);
-        printSelected.addActionListener(e -> {
+        Button substitutePlayer = new Button("Substitute Player");
+        substitutePlayer.setBackground(Color.BLUE);
+        substitutePlayer.setForeground(Color.WHITE);
+        substitutePlayer.addActionListener(e -> {
             System.out.println("Player out: " + startingEleven.getSelection().getActionCommand());
             System.out.println("Player in: " + substitutes.getSelection().getActionCommand());
 
-            userTeam.makeSubstitution(userTeam.getPlayerByName(startingEleven.getSelection().getActionCommand()), userTeam.getPlayerByName(substitutes.getSelection().getActionCommand()));
-            this.dispose();
+            try {
+                if (userTeam.getPlayerByName(startingEleven.getSelection().getActionCommand()).getPosition().equals(userTeam.getPlayerByName(substitutes.getSelection().getActionCommand()).getPosition())){
+                    userTeam.makeSubstitution(userTeam.getPlayerByName(startingEleven.getSelection().getActionCommand()), userTeam.getPlayerByName(substitutes.getSelection().getActionCommand()));
+                    this.dispose();
 
-            new manageTeam(userTeam);
+                    new manageTeam(userTeam);
+                } else {
+                    throw new IllegalArgumentException("Players must be the same position to make a substitution.");
+                }
+            } catch (IllegalArgumentException exception) {
+                System.out.println(exception.getMessage());
+                new JOptionPane().showMessageDialog(null, exception.getMessage());
+            }
+
         });
 
-        startingElevenPanel.add(printSelected);
+        startingElevenPanel.add(substitutePlayer);
 
         this.add(startingElevenPanel);
         this.add(benchPanel);
