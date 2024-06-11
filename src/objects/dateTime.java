bf3869e68759b9d3b03700d2b9ae6acb219e742d
Ben Hockley <hockleyb@cardiff.ac.uk>
2024-06-11 08:20:48 +0100

    Simplified dateTime

--- src/objects/dateTime.java
+++ src/objects/dateTime.java
@@ -1,7 +1,17 @@
 package objects;
 
 public class dateTime {
-    //contains functions for updating the in-game calendar.
+    /**
+     * The lengths of each month in days.
+     */
+    private final Integer[] monthLengths =
+            new Integer[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    /**
+     * The names of each month.
+     */
+    private final String[] monthNames =
+            new String[]{"January", "February", "March", "April", "May", "June",
+                    "July", "August", "September", "October", "November", "December"};
     /**
      * The day of the month, increments by 1 each day, and resets to one
      * at the end of the month.
@@ -17,92 +27,68 @@ public class dateTime {
      */
     private Integer yearNumber;
     /**
-     * The number of days in the current month, dependant by monthNumber.
+     * Creates a new dateTime object with the given starting date.
+     * Used to feed the calendarPanel to the north of the main UI.
+     * @param startingDate The starting date of the simulation.
      */
-    private Integer monthLength;
-    /**
-     * The name of the current month, dependant by monthNumber.
-     */
-    private String monthName;
-
-    public dateTime(Integer[] startingDate){
+    public dateTime(final Integer[] startingDate) {
         this.dateNumber = startingDate[0];
         this.monthNumber = startingDate[1];
         this.yearNumber = startingDate[2];
-
-        this.monthLength = setMonthLength(monthNumber);
-        this.monthName = setMonthName(monthNumber);
     }
 
-    private Integer setMonthLength(Integer monthNumber){
-        if (monthNumber == 1 || monthNumber == 3 || monthNumber == 5 || monthNumber == 7 || monthNumber == 8 || monthNumber == 10 || monthNumber == 12){
-            monthLength = 31;
-        } else if (monthNumber == 2){
-            monthLength = 28;
-        } else {
-            monthLength = 30;
-        }
-        return monthLength;
-    }
-    private String setMonthName(Integer monthNumber){
-        if (monthNumber == 1){
-            monthName = "January";
-        } else if (monthNumber == 2){
-            monthName = "February";
-        } else if (monthNumber == 3){
-            monthName = "March";
-        } else if (monthNumber == 4){
-            monthName = "April";
-        } else if (monthNumber == 5){
-            monthName = "May";
-        } else if (monthNumber == 6){
-            monthName = "June";
-        } else if (monthNumber == 7){
-            monthName = "July";
-        } else if (monthNumber == 8){
-            monthName = "August";
-        } else if (monthNumber == 9){
-            monthName = "September";
-        } else if (monthNumber == 10){
-            monthName = "October";
-        } else if (monthNumber == 11){
-            monthName = "November";
-        } else if (monthNumber == 12){
-            monthName = "December";
-        } else {
-            monthName = "January";
-        }
-        return monthName;
-    }
-
-    public void progressDate(){
+    /**
+     * Increments the date by one day.
+     * Resets month/ increments year if necessary.
+     */
+    public void progressDate() {
         this.dateNumber += 1;
-        if (dateNumber > monthLength){
+
+        int monthLength = monthLengths[monthNumber - 1];
+        if (dateNumber > monthLength) {
             this.dateNumber = 1;
             this.monthNumber += 1;
-            setMonthLength(monthNumber);
-            setMonthName(monthNumber);
         }
-        if (monthNumber > 12){
+        if (monthNumber > 12) {
             this.monthNumber = 1;
             this.yearNumber += 1;
         }
     }
 
-    public Integer getDateNumber(){
+    /**
+     * returns the date of the month.
+     * @return the date of the month (e.g. 5)
+     */
+    public Integer getDateNumber() {
         return dateNumber;
     }
-    public Integer getMonthNumber(){
+    /**
+     * returns the month of the year.
+     * @return the month of the year (e.g. 3)
+     */
+    public Integer getMonthNumber() {
         return monthNumber;
     }
-    public Integer getYearNumber(){
+
+    /**
+     * returns the year of the simulation.
+     * @return the year of the simulation (e.g. 2021)
+     */
+    public Integer getYearNumber() {
         return yearNumber;
     }
-
-    public Integer getMonthLength(){
-        return monthLength;
+    /**
+     * returns the length of the current month, from the list of month lengths.
+     * @return the length of the current month (e.g. 31)
+     */
+    public Integer getMonthLength() {
+        return monthLengths[monthNumber - 1];
     }
-    public String getMonthName(){
-        return monthName;
+    /**
+     * returns the name of the current month, from the list of month names.
+     * @return the name of the current month (e.g. "March")
+     */
+    public String getMonthName() {
+        return monthNames[monthNumber - 1];
     }
 }
